5a265ded1190c63b5f58a0390e10542cc3c546e4
domogami <d3lee@ucsd.edu>
2021-03-07 15:18:45 -0800

    boat

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,45 @@ import * as React from "react"
 import { Link } from "gatsby"
 import "./header.css"
 
-const Header = () => (
-  <header>
-    <div className="bar">
-      <Link className="menuItem" to="/"><p># TODO</p></Link>
-      <Link className="menuItem" to="/#about"><p>Summary</p></Link>
-      <Link className="menuItem" to="/analysis"><p>Project</p></Link>
-      <Link className="menuItem" to="/#conclusion"><p>Conclusion</p></Link>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Header
+    this.state = {
+      hasScrolled: false,
+      num: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true });
+    } else {
+      this.setState({ hasScrolled: false });
+    }
+  };
+
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      > 
+        <div className="HeaderGroup">
+          <a href="/"># TODO</a>
+          <a href="/#about">Summary</a>
+          <Link to="/analysis">Project</Link>
+          <a href="#conclusion">Conclusion</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
